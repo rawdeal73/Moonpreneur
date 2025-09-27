fe0a6e0e0587b5b9685f86dc4b706d4a05e983f4
TheReal Rawdeal <30921727+rawdeal73@users.noreply.github.com>
2025-09-27 15:31:30 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+basic.forever(function () {
+    basic.showLeds(`
+        # . # . #
+        # # # # #
+        . . # . .
+        . # # # .
+        # # . # #
+        `)
+    basic.showLeds(`
+        . . # . .
+        # # # # #
+        # . # . #
+        . # # # .
+        # # . # #
+        `)
+    basic.pause(1000)
+})
